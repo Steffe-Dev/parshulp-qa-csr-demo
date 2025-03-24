4f4721ac882679f37b9387b2fbfed6c951fa5181
Frans Stefanus Bothma <fstefanusbothma@gmail.com>
2025-03-24 17:35:07 +0200

    Try deployment again

--- src/components/counter_btn.rs
+++ src/components/counter_btn.rs
@@ -1,15 +1,10 @@
-use leptos::prelude::*;
+use leptos::{ev, html::*, prelude::*};
 
 /// A parameterized incrementing button
 #[component]
 pub fn Button(#[prop(default = 1)] increment: i32) -> impl IntoView {
     let (count, set_count) = signal(0);
-    view! {
-        <button on:click=move |_| {
-            set_count.set(count.get() + increment)
-        }>
-
-            "Click me: " {count}
-        </button>
-    }
+    button()
+        .on(ev::click, move |_| set_count.set(count.get() + increment))
+        .child(("Click me: ", count))
 }
